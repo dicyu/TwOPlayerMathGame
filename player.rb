e550163f3dd552beb9ab1a7376b9260af4edf95f
Dickson Yu <dickson.yu@email.com>
2022-01-31 15:57:51 -0500

    Adding outputing question to the user

--- player.rb
+++ player.rb
@@ -1,17 +1,30 @@
 class Player
-  attr_reader :name :lives
+  attr_reader :name, :lives
 
   def initialize(name)
-    @name = name
+    @name = name,
     @lives = 3
   end
 
-  def lose_lose
+  def lose_life
     @lives -= 1
   end
 
   def lost_game
     @lives == 0
   end
-  
+
+  def next_question
+    next_question = Questions.new
+    next_question.math_problem(name)
+    print '>'
+    @choice = $stdin.gets.chomp
+
+    if next_question.checking_answer?(@choice.to_i)
+      puts "Correct."
+    else
+      puts 'Wrong.'
+      lose_life
+    end
+  end
 end
